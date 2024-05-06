5d0039fee4096168d780b932082ed143ec227510
Tony <antonivelasquez67@gmail.com>
2024-05-05 22:14:38 -0400

    TODO: CSSING

--- src/Object.js
+++ src/Object.js
@@ -1,13 +1,18 @@
 const btnTodo = document.querySelector("#btn-todo");
-
+const dialog = document.querySelector("dialog");
 
 btnTodo.addEventListener("click", () => {
-  const formTodo = document.createElement("div");
-  formTodo.className = "inputTodo";
-  const input1 = document.createElement("input");
-  formTodo.appendChild(input1);
-  btnTodo.appendChild(formTodo);
-  //inputTodo.replaceChildren(input1)
+  const formTodo = document.createElement("form");
+  formTodo.setAttribute("method", "Dialog");
+  formTodo.className = "formTodo";
+  const title = document.createElement("input");
+  title.setAttribute("type", "text");
+  title.setAttribute("name", "title");
+  title.setAttribute("id", "title");
+  title.setAttribute("placeholder", "Title");
+  dialog.appendChild(formTodo);
+  formTodo.appendChild(title);
+  //inputTodo.replaceChildren(title)
 })
 
 function objCreator(title, description, priority, dueDate) {
